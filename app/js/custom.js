5f0c865e9af4a237a5515e9b999329f539e07cf6
baudeveloper2 <glomarkl@baustein.ca>
2018-01-02 09:46:31 -0500

    first commit - test

---
+++ app/js/custom.js
@@ -0,0 +1,25 @@
+//========================
+// Strict Syntax Rendering
+//========================
+"use strict";
+
+//==================
+// Application Logic
+//==================
+var myApp = {};
+myApp.init = () => {
+	// const SLANG = "Eh! ";
+	// const SPEAK = "Today, The weather is: ";
+	// let weather = 100;
+  // document.querySelector(".msg").textContent = SLANG + SPEAK + weather;
+	$("a[data-js=\"signup\"]").on("click", function() {
+		alert("You clicked me.");
+	});
+};
+
+//===============
+// Initialization
+//===============
+$(function() {
+	myApp.init();
+});
